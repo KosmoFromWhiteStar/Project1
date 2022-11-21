5e13bb542fac75cee4b89522cfdba2925ed97c2a
Kosmo <The_Kosmo@outlook.com>
2022-11-21 22:59:34 +0500

    worked

--- ex8.cpp
+++ ex8.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <cassert>
 
+//void fight(bool battlefield[][10], bool shoots[][10])
+//{
+//	std::cout << std::endl;
+//	for (int y = -2; y < 10; y++)
+//	{
+//		for (int x = -2; x < 10; x++)
+//		{
+//			if (!(x == -2 && y == -2))
+//			{
+//				if (y < -1 && x < 0)std::cout << " ";
+//				if (y < 0 && x == -1)std::cout << "  ";
+//				if (x == -1 && y == -1)std::cout << "+";
+//				if (y == -2 && x >= 0) std::cout << " " << x << "  ";
+//				else if (y == -1 && x >= 0) std::cout << "----";
+//				if (x == -2 && y >= 0) std::cout << y << ' ';
+//				else if (x == -1 && y >= 0) std::cout << "|";
+//
+//				if (x >= 0 && y >= 0)
+//				{
+//					bool tempo = shoots[x][y];
+//					if (battlefield[x][y] && shoots[x][y])std::cout << " X  ";
+//					else if (!battlefield[x][y] && shoots[x][y]) std::cout << " M  ";
+//					else std::cout << " 0  ";
+//				}
+//			}
+//		}
+//		std::cout << std::endl;
+//	}
+//	std::cout << std::endl;
+//}
 void display_(bool battlefield[][10])
 {
 	std::cout << std::endl;
@@ -7,9 +38,9 @@ void display_(bool battlefield[][10])
 	{
 		for (int x = -2; x < 10; x++)
 		{
-			if (!(x == -2 && y == -2)) 
+			if (!(x == -2 && y == -2))
 			{
-				if (y < -1 && x < 0 )std::cout << " ";
+				if (y < -1 && x < 0)std::cout << " ";
 				if (y < 0 && x == -1)std::cout << "  ";
 				if (x == -1 && y == -1)std::cout << "+";
 				if (y == -2 && x >= 0) std::cout << " " << x << "  ";
@@ -23,6 +54,57 @@ void display_(bool battlefield[][10])
 	}
 	std::cout << std::endl;
 }
+
+void battle(bool battledield_p1[][10], bool battledield_p2[][10], std::string name[2])
+{
+	bool shoots_p1[10][10]; // compare with battlefield [0] = 1st. [1] = 2nd
+	bool shoots_p2[10][10];
+	int position[2] = { 0, 0 };
+	int coordinate_shoots[2] = { 0, 0 };
+	bool tempo = false;
+	int i = 0;
+
+	for (int y = 0; y < 10; y++) {
+		for (int x = 0; x < 10; x++)
+		{
+			shoots_p1[x][y] = false;
+			if (battledield_p2[x][y]) position[0] += 1;
+			shoots_p2[x][y] = false;
+			if (battledield_p1[x][y]) position[1] += 1;
+		}
+	}
+	assert(position[0] == position[1]);
+	while (position[0] != 0 || position[1] != 0)
+	{
+		std::cout << (i == 0 ? "1st" : "2nd") << " player shoots : ";
+		std::cin >> coordinate_shoots[0] >> coordinate_shoots[1];
+
+		if (i == 0 && !shoots_p1[coordinate_shoots[0]][coordinate_shoots[1]])
+		{
+			shoots_p1[coordinate_shoots[0]][coordinate_shoots[1]] = true; // не присваевает значение тру
+			position[i]--;
+			tempo = shoots_p1[coordinate_shoots[0]][coordinate_shoots[1]];
+		}
+
+
+		if (i == 1 && !shoots_p2[coordinate_shoots[0]][coordinate_shoots[1]])
+		{
+			shoots_p2[coordinate_shoots[0]][coordinate_shoots[1]] = true;
+			position[i]--;
+		}
+		//fight(battledield_p1, shoots_p1);
+
+		std::cout << std::endl;
+		i++;
+		i = i % 2;
+		display_(shoots_p1);
+		display_(shoots_p2);
+	}
+	std::cout << " Win " << (position[0] > position[1] ? name[0] : name[1]) << ". Congratulete.";
+}
+
+
+
 void arrange_ship(bool sea_battle[][10])
 {
 	int coordinata[4];
@@ -169,16 +251,22 @@ int main()
 	 2 средних корабля размером в три клетки 
 	 и один большой корабль размером в четыре клетки
 	*/
-	/*std::cout << "Sea Battle!!!\a\n\nInput name first player:";
-	std::string p1;
-	std::cin >> p1;
-	std::cout << "\nInput snd player: ";
-	std::string p2;
-	std::cin >> p2;*/
 
-	std::cout <</* p1 <<*/ " arrange ships;\nYou have 4 [I], 3 [II], 2 [III], 1[IIII]\n";
+	std::cout << "Sea Battle!!!\a\n\nInput name first player:";
+	std::string p[2];
+	std::cin >> p[0];
+	std::cout << "\nInput snd player: ";
+	std::cin >> p[1];
+
+	std::cout << p[0] <<  " arrange ships;\nYou have 4 [I], 3 [II], 2 [III], 1[IIII]\n";
 	arrange_ship(sea_battle_field_p1);
-	//arrange_ship(sea_battle_field_p2);
+
+	std::cout << p[1] << " arrange ships;\nYou have 4 [I], 3 [II], 2 [III], 1[IIII]\n";
+	arrange_ship(sea_battle_field_p2);
+
+	std::cout << "Start battle!!\n";
+	battle(sea_battle_field_p1, sea_battle_field_p2, p);
+
 
 
 
